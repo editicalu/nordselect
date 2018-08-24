60fc1fa80f0825d23a8c2033b54a3d7029eb7054
Ward Segers <w@rdsegers.be>
2018-08-24 20:45:04 +0200

    Other filters

--- src/filters.rs
+++ src/filters.rs
@@ -15,12 +15,128 @@ pub struct CountryFilter {
 
 impl CountryFilter {
     pub fn from_code(country: String) -> CountryFilter {
-        CountryFilter { country }
+        CountryFilter {
+            country: country.to_ascii_uppercase(),
+        }
     }
 }
 
 impl Filter for CountryFilter {
     fn filter(&self, server: &Server) -> bool {
         self.country == server.flag
+    }
+}
+
+/// Filter that keeps servers from any of the provided countries.
+pub struct CountriesFilter {
+    countries: Vec<String>,
+}
+
+/// Region operations
+impl CountriesFilter {
+    /// Builds a CountriesFilter from one of the provided regions.
+    pub fn from_region(region: &str) -> Option<CountriesFilter> {
+        match region.to_lowercase().as_ref() {
+            "eu" | "ею" => Some(CountriesFilter {
+                countries: vec![
+                    String::from("AT"),
+                    String::from("BE"),
+                    String::from("BG"),
+                    String::from("HR"),
+                    String::from("CY"),
+                    String::from("CZ"),
+                    String::from("DK"),
+                    String::from("EE"),
+                    String::from("FI"),
+                    String::from("FR"),
+                    String::from("DE"),
+                    String::from("GR"),
+                    String::from("HU"),
+                    String::from("IE"),
+                    String::from("IT"),
+                    String::from("LV"),
+                    String::from("LT"),
+                    String::from("LU"),
+                    String::from("MT"),
+                    String::from("NL"),
+                    String::from("PL"),
+                    String::from("PT"),
+                    String::from("RO"),
+                    String::from("SK"),
+                    String::from("SI"),
+                    String::from("ES"),
+                    String::from("SE"),
+                ],
+            }),
+            _ => None,
+        }
+    }
+
+    /// Returns regions that can be used
+    pub fn available_regions() -> Vec<&'static str> {
+        vec!["eu", "ею"]
+    }
+}
+
+impl From<Vec<String>> for CountriesFilter {
+    fn from(countries: Vec<String>) -> CountriesFilter {
+        CountriesFilter { countries }
+    }
+}
+
+impl Filter for CountriesFilter {
+    fn filter(&self, server: &Server) -> bool {
+        self.countries.contains(&server.flag)
+    }
+}
+
+/// Filter that keeps servers that accept a specific protocol.
+pub struct ProtocolFilter {}
+
+/// Filter that keeps servers with less load than a provided value.
+pub struct LoadFilter {}
+
+/// Filter that contains multiple Filter instances. This could be more efficient, as only servers fullfilling all requirements are kept.
+pub struct CombinedFilter {
+    filters: Vec<Box<Filter>>,
+}
+
+/// Ways to construct `CombinedFilters`.
+impl CombinedFilter {
+    /// Builds a new `CombinedFilter`.
+    pub fn new() -> CombinedFilter {
+        CombinedFilter {
+            filters: Vec::new(),
+        }
+    }
+
+    /// Builds a new `CombinedFilter` with the given capacity.
+    pub fn with_capacity(capacity: usize) -> CombinedFilter {
+        CombinedFilter {
+            filters: Vec::with_capacity(capacity),
+        }
+    }
+}
+
+impl From<Vec<Box<Filter>>> for CombinedFilter {
+    fn from(source: Vec<Box<Filter>>) -> CombinedFilter {
+        CombinedFilter { filters: source }
+    }
+}
+
+impl CombinedFilter {
+    /// Adds a new filter
+    pub fn add_filter(&mut self, filter: Box<Filter>) {
+        self.filters.push(filter);
+    }
+}
+
+impl Filter for CombinedFilter {
+    fn filter(&self, server: &Server) -> bool {
+        self.filters
+            .iter()
+            .filter(|filter| filter.filter(server))
+            .next()
+            .is_some()
     }
 }
